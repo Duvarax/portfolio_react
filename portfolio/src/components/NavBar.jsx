e400c220e5f0f71a6a085b6f8519dcb019775edf
Duvara Claudio <91039016+Duvarax@users.noreply.github.com>
2024-01-17 15:44:05 -0300

    navbar

---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+//import './navbar.css'
+function NavBar() {
+  return (
+    <nav className='sm:container mx-auto rounded-lg border border-violet-800 bg-violet-950'>NavBar</nav>
+  )
+}
+
+export default NavBar
