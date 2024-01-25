3709f0883fe18e59a259882095976b85bd51d3ed
Duvara Claudio <91039016+Duvarax@users.noreply.github.com>
2024-01-25 15:37:16 -0300

    button

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavButton from './NavButton'
 //import './navbar.css'
 function NavBar() {
   return (
-    <nav className='sm:container mx-auto rounded-lg border border-violet-800 bg-violet-950'>NavBar</nav>
+    <nav className='sm:container mx-auto rounded-lg border border-violet-800 bg-violet-950 flex gap-5 justify-end p-1'>
+      <NavButton text='boton1'/>
+      <NavButton text='boton2'/>
+      <NavButton text='boton3'/>
+    </nav>
   )
 }
 
---
+++ portfolio/src/components/NavButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavButton({text}) {
+  return (
+    <button className='bg-slate-50 rounded px-1.5 font-bold '>{text}</button>
+  )
+}
+
+export default NavButton
